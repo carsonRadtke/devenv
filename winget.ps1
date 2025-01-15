49a29ec8d96a3f13ff416e9d8932f4ede714608e
Carson Radtke <carsonRadtke@users.noreply.github.com>
2025-01-15 10:28:15 -0600

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,23 @@
+winget install BurntSushi.ripgrep.MSVC
+winget install GNU.Wget2
+winget install GitHub.cli
+winget install Kitware.CMake
+winget install mbuilov.sed
+winget install Neovim.Neovim
+
+winget install GnuWin32.DiffUtils
+winget install GnuWin32.FindUtils
+winget install GnuWin32.Grep
+winget install GnuWin32.Which
+
+winget install LLVM.ClangFormat
+winget install LLVM.clangd
+winget install LLVM.LLVM
+
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.PowerToys
+winget install Microsoft.Git
+winget install Microsoft.WindowsTerminal
+
+winget install sharkdp.bat
+winget install sharkdp.fd
