2b9bd5714fd0ac0af9b849bae7a710d5cdfae100
Carson Radtke <nosrac925@gmail.com>
2025-08-10 13:44:12 -0600

    more updates to tmux and nvim

--- init.lua
+++ init.lua
@@ -13,6 +13,9 @@ vim.opt.autoindent = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '→ ', trail = '·', nbsp = '␣' }
 
+-- Fix tmux warning
+vim.opt.termguicolors = true
+
 -- Persistent undo
 vim.opt.undofile = true
 vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undo"
@@ -26,3 +29,27 @@ vim.opt.backspace = { "indent", "eol", "start" }
 -- Auto-reload files changed outside vim
 vim.opt.autoread = true
 
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Global keybindings
+keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+
+-- Buffer-local keybindings in on_attach function
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+end
+
+local lspconfig = require('lspconfig')
+
+lspconfig.clangd.setup{
+  on_attach = on_attach
+}
